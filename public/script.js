c94d9b2a4a51363c01d79bdd7d3d0d89c0bf56db
lidia-ixd <72181243+lidia-ixd@users.noreply.github.com>
2021-04-29 13:14:32 -0400

    first commit

---
+++ public/script.js
@@ -0,0 +1,13 @@
+
+// slide //
+$(document).ready(function () {
+$("#flip").click(function () {
+$("#slide").slideToggle("fast");
+ });
+});
+
+  $( function() {
+    $( "#accordion" ).accordion({
+      collapsible: true
+    });
+  } );
